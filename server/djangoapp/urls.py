454b9949460ec9c851646e5ba729451b6e6a8137
Peeyush-Vatsa <dev.pvatsa@gmail.com>
2021-07-21 19:17:35 +0530

    Major design changes to the navbar. Adding login and signup capabilities

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,14 +10,15 @@ urlpatterns = [
     # name the URL
 
     # path for about view
-    path(route='about_us', view=views.about, name='about_us'),
+    path(route='about_us/', view=views.about, name='about_us'),
     # path for contact us view
-    path(route='contact_us', view=views.contact, name='contact_us'),
+    path(route='contact_us/', view=views.contact, name='contact_us'),
     # path for registration
-
+    path(route='sign_up/', view=views.registration_request, name='register'),
     # path for login
-
+    path(route='login/', view=views.login_request, name='login'),
     # path for logout
+    path(route='logout/', view=views.logout_request, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
 
