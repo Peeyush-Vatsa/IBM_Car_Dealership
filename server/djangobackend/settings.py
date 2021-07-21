661fd0c8db71e30422cbae43ebad10fbb4a74f00
Peeyush-Vatsa <dev.pvatsa@gmail.com>
2021-07-21 12:24:17 +0530

    Updating route host on IBM Cloud

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost", "dev-pvatsa-3f.eu-gb.mybluemix.net"]
+ALLOWED_HOSTS = ["localhost", "CD1028-3bd-dev-pv.eu-gb.mybluemix.net"]
 
 
 # Application definition
